d8af81e08a2088e6d2ab4b441c45edb207609c64
drMimic <gartflores@gmail.com>
2020-08-20 15:41:02 -0400

    combatController instantiated in BaseCharacter, protected params added / combatController functionality setup first attempt

--- BaseCharacter.cs
+++ BaseCharacter.cs
@@ -1,32 +1,42 @@
 using UnityEngine;
 
-public class BaseCharacter {
+public class BaseCharacter : MonoBehabiour {
 
   [HideInInspector]
-  public float health;
+  protected float health = 100;
 
   [SerializeField]
-  private float initHealth;
+  protected float initHealth = 100;
 
   [SerializeField]
-  private float hpRegeneratePerSecond;
+  protected float hpRegeneratePerSecond = 0;
 
   [HideInInspector]
-  public bool isDead;
+  public bool isDead = false;
+
+  private CombatController combatController;
 
   private void Init() {
 
   }
 
   private void Start() {
-
+    combatController = new CombatController( this );
   }
 
   private void LateUpdate() {
 
   }
 
+  public void TakeDamage( float dmg ) {
+    health -= dmg;
+    if ( health <= 0 ) {
+      Die();
+    }
+  }
+
   private void Die() {
-    
+
+    isDead = true;
   }
 }
--- CombatController.cs
+++ CombatController.cs
@@ -1,15 +1,33 @@
+using System.Collections;
+
 public class CombatController {
 
+  private BaseCharacter character;
+  private Coroutine debuffCoroutine;
+
+  public CombatController( BaseCharacter character ) {
+    character = character;
+  }
+
   public void ApplyDamage( float dmg ) {
-
+    character.TakeDamage( dmg );
   }
 
-  public void ApplyDebuff( float numA, float numB ) {
-
+  public void ApplyDebuff( float dmg, float time ) {
+    debuffCoroutine = character.StartCoroutine( Debuff( dmg, time ) );
   }
 
-  private IEnumerator Debuff( float num ) {
-    
+  private IEnumerator Debuff( float dmg, float time ) {
+    float dmgTick = dmg / time;
+    float dmgTaken = 0;
+    while (dmgTaken < dmg) {
+      ApplyDamage( dmgTick );
+      dmgTaken += dmgTick;
+      if ( character.isDead ) {
+        character.StopCoroutine( debuffCoroutine );
+      }
+      yield return new WaitForSeconds( 1f );
+    }
   }
 
 }
